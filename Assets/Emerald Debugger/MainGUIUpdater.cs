39de7dbfed63806eae37635b5e45e587090ac152
SlamAKnife <67249829+SlamAKnife@users.noreply.github.com>
2020-07-26 18:46:04 -0400

    Something i actually need

---
+++ Assets/Emerald Debugger/MainGUIUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGUIUpdater : MonoBehaviour
+{
+
+    public Text timeDeltaDisp;
+    public Text fpsDisp;
+    public Text afpsDisp;
+    public int afpsCount;
+    public float afpsGuide;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeDeltaDisp.text = "Time Delta:  " + (Time.deltaTime).ToString();
+        fpsDisp.text = "FPS:  " + Mathf.RoundToInt(Time.deltaTime * 3600f).ToString();
+        afpsCount += 1;
+        afpsGuide += Time.deltaTime * 3600f;
+        afpsDisp.text = "Average FPS:  " + Mathf.RoundToInt(afpsGuide / afpsCount).ToString();
+    }
+}
